f0f7630d2206f1216fb3f1807de20ca296dad046
dfh1949 <88699201+dfh1949@users.noreply.github.com>
2025-09-25 17:23:27 +0800

    Update custom.sh

--- custom.sh
+++ custom.sh
@@ -6,9 +6,9 @@ lan_ip="10.0.1"                                 # LAN 地址 别写后面的 .1
 wlan_2g_ssid="int_2.4G"                             # 2G 无线名称
 wlan_5g_ssid="int_5G"                               # 5G 无线名称
 wlan_guest_2g_ssid="li123789"                   # 2G 访客无线名称
-wlan_guest_5g_ssid="int_pub_5G"                     # 5G 访客无线名称
+# wlan_guest_5g_ssid="int_pub_5G"                     # 5G 访客无线名称
 wlan_2g_psk="li518518"                              # 2G WIFI密码最少8位 空白为不设置
-wlan_5g_psk="qwerasdf"                              # 5G WIFI密码最少8位 空白为不设置
+# wlan_5g_psk="qwerasdf"                              # 5G WIFI密码最少8位 空白为不设置
 version_time=$(date +%Y%m%d)                        # 更新时版本号时间: 20210101
 default_file="./user/shared/defaults.h"             # 默认配置文件
 
@@ -19,25 +19,25 @@ echo "修改登陆密码"
 sed -i 's/DEF_ROOT_PASSWORD	"admin"/DEF_ROOT_PASSWORD	"'$user_password'"/g' $default_file
 
 echo "修改 LAN IP 地址"
-sed -i "s/192.168.2/$lan_ip/g" $default_file
+sed -i "s/10.0.1/$lan_ip/g" $default_file
 
 echo "修改 2G 无线名称"
 sed -i 's/DEF_WLAN_2G_SSID	BOARD_PID "_%s"/DEF_WLAN_2G_SSID	"'$wlan_2g_ssid'"/g' $default_file
 
-#echo "修改 5G 无线名称"
-#sed -i 's/DEF_WLAN_5G_SSID	BOARD_PID "_5G_%s"/DEF_WLAN_5G_SSID	"'$wlan_5g_ssid'"/g' $default_file
+# echo "修改 5G 无线名称"
+# sed -i 's/DEF_WLAN_5G_SSID	BOARD_PID "_5G_%s"/DEF_WLAN_5G_SSID	"'$wlan_5g_ssid'"/g' $default_file
 
 echo "修改 2G 访客无线名称"
 sed -i 's/DEF_WLAN_2G_GSSID	BOARD_PID "_GUEST_%s"/DEF_WLAN_2G_GSSID	"'$wlan_guest_2g_ssid'"/g' $default_file
 
-#echo "修改 5G 访客无线名称"
-#sed -i 's/DEF_WLAN_5G_GSSID	BOARD_PID "_GUEST_5G_%s"/DEF_WLAN_5G_GSSID	"'$wlan_guest_5g_ssid'"/g' $default_file
+# echo "修改 5G 访客无线名称"
+# sed -i 's/DEF_WLAN_5G_GSSID	BOARD_PID "_GUEST_5G_%s"/DEF_WLAN_5G_GSSID	"'$wlan_guest_5g_ssid'"/g' $default_file
 
 echo "修改 2.4GHz WIFI 密码"
 sed -i 's/DEF_WLAN_2G_PSK		"1234567890"/DEF_WLAN_2G_PSK		"'$wlan_2g_psk'"/g' $default_file
 
-#echo "修改 5GHz WIFI 密码"
-#sed -i 's/DEF_WLAN_5G_PSK		"1234567890"/DEF_WLAN_5G_PSK		"'$wlan_5g_psk'"/g' $default_file
+# echo "修改 5GHz WIFI 密码"
+# sed -i 's/DEF_WLAN_5G_PSK		"1234567890"/DEF_WLAN_5G_PSK		"'$wlan_5g_psk'"/g' $default_file
 
 echo "更新版本号时间"
 sed -i "s/FIRMWARE_BUILDS_REV=.*/FIRMWARE_BUILDS_REV=$version_time/g" ./versions.inc
@@ -56,8 +56,8 @@ default_path="./user/shared"                    # 默认配置路径
 #echo "设置为 AP模式"
 #echo "#define BOARD_ROLE_REPEATER	1" >> ./configs/boards/RM2100/board.h
 
-#echo "打开2G无线桥接"
-#sed -i 's/{ "rt_mode_x", "0" }/{ "rt_mode_x", "4" }/g' $default_path/defaults.c
+# echo "打开2G无线桥接"
+# sed -i 's/{ "rt_mode_x", "0" }/{ "rt_mode_x", "4" }/g' $default_path/defaults.c
 
-#echo "打开5G无线桥接"
-#sed -i 's/{ "wl_mode_x", "0" }/{ "wl_mode_x", "4" }/g' $default_path/defaults.c
+# echo "打开5G无线桥接"
+# sed -i 's/{ "wl_mode_x", "0" }/{ "wl_mode_x", "4" }/g' $default_path/defaults.c
